bcce4f1b6e521dc71510b35481b53f95cbee3ed5
Şamil Bayyurt <35709507+SamBayy@users.noreply.github.com>
2021-06-03 14:08:25 +0300

    Update museum.py

--- Loc/museum.py
+++ Loc/museum.py
@@ -5,8 +5,8 @@ import numpy as np
 colorNone = np.array([[0, 0, 0], [0, 0, 0]])
 # color1 = np.array([[180, 255, 30], [0, 0, 0]])
 # color1 = np.array([[27, 197, 255], [21, 107, 140]])
-color1 = np.array([[33, 184, 255], [26, 126, 145]])
-color2 = np.array([[180, 18, 255], [0, 0, 231]])
+color1 = np.array([[255, 255, 255], [62, 160, 180]])
+color2 = np.array([[255, 255, 255], [160, 94, 140]])
 
 color_dict_HSV = {'None': colorNone,
                   'color1': color1,
@@ -14,8 +14,8 @@ color_dict_HSV = {'None': colorNone,
 
 
 ###########################################
-total_x = 280  # this value must be changed
-total_y = 210  # this value must be changed
+total_x = 480  # this value must be changed
+total_y = 350  # this value must be changed
 ###########################################
 
 def pixels_to_cms(current_pixel, all_pixels, total_length):
@@ -41,15 +41,15 @@ class Visitor:
         self.sections = sections
         self.visit_map = visit_map
         self.found = False
-        self.x = 0
-        self.y = 0
+        self.x = -500
+        self.y = -500
 
     def change_color(self, new_color):
         self.color = color_dict_HSV[new_color]
 
     def track(self, frame, hsv):
-        x = -1
-        y = -1
+        x = -500
+        y = -500
         pts = deque(maxlen=50)
         krnl = np.ones((4, 4), np.uint8)  # burada yapıyor
         # masking
@@ -89,8 +89,8 @@ class Visitor:
 
 
         self.found = found
-        self.x = pixels_to_cms(x, frame.shape[0], total_x)
-        self.y = pixels_to_cms(y, frame.shape[1], total_y)
+        self.x = x
+        self.y = y
 
         # appending centers
         pts.appendleft(center)
@@ -106,10 +106,19 @@ class Visitor:
 class Items:
     def __init__(self,
                  x=None,
-                 y=None):
+                 y=None,
+                 x_cm=None,
+                 y_cm=None,
+                 frame=None):
         if x is None:
             x = 0
         if y is None:
             y = 0
-        self.x = x  # in cms
-        self.y = y  # in cms
+        if x_cm is None:
+            x_cm = 0
+        if y_cm is None:
+            y_cm = 0
+        self.x_cm = (x/480) * total_x
+        self.y_cm = (y/640) * total_y
+        self.x = x
+        self.y = y
