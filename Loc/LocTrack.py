1162509c1879d33ce91fcdf80a5f02c0f1028e87
Şamil Bayyurt <35709507+SamBayy@users.noreply.github.com>
2021-06-03 14:07:54 +0300

    Update LocTrack.py

--- Loc/LocTrack.py
+++ Loc/LocTrack.py
@@ -10,13 +10,13 @@ def item_vs_visitors(item, visitor_x, visitor_y):
     distance = 0.00
     distance = (item.x-visitor_x)*(item.x-visitor_x)+(item.y-visitor_y)*(item.y-visitor_y)
     distance = math.sqrt(distance)
-    if distance < 50:
+    if distance < 50*1.8:
         return True
     else:
         return False
 
 # murat burayi duzenle!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-def choose_recordings(x, y, item1, item2, item3, door):
+def choose_recordings(x, y, item1, item2, item3, door1, door2):
     close_to_this = 0
     if item_vs_visitors(item1, x, y): # is it close to item1
         close_to_this = 1  # esp1 = "item1.m4a"
@@ -24,22 +24,25 @@ def choose_recordings(x, y, item1, item2, item3, door):
         close_to_this = 2  # esp1 = "item2.m4a"
     elif item_vs_visitors(item3, x, y): # is it close to item3
         close_to_this = 3  # esp1 = "item3.m4a"
-    elif item_vs_visitors(item3, x, y):
+    elif item_vs_visitors(door1, x, y):
         close_to_this = 4 # visitor at the door; play 'you are not allowed'.
+    elif item_vs_visitors(door2, x, y):
+        close_to_this = 5 # visitor at the door; play 'you are not allowed'.
     else:
         close_to_this = 0  # esp1 = "item2.m4a"  # no record will be played!!!!!!!!!!! it is not close to any item
     return close_to_this
 
 
 ###########################################
-total_x = 280  # this value must be changed
-total_y = 210  # this value must be changed
+total_x = 480  # this value must be changed
+total_y = 350  # this value must be changed
 ###########################################
 
-item1 = museum.Items(20, 180)  # this values must be changed
-item2 = museum.Items(250, 180)  # this values must be changed
-item3 = museum.Items(130, 20)  # this values must be changed
-door1 = museum.Items(0,0) #tam kosede    
+item1 = museum.Items(47, 432)  # this values must be changed
+item2 = museum.Items(593, 432)  # this values must be changed
+item3 = museum.Items(320, 47)  # this values must be changed
+door1 = museum.Items(47, 47) #tam kosede
+door2 = museum.Items(593, 47) #unknown
 
 visitor1 = museum.Visitor('Visitor 1')
 visitor1.change_color('color1')
@@ -47,12 +50,16 @@ visitor1.change_color('color1')
 visitor2 = museum.Visitor('Visitor 2')
 visitor2.change_color('color2')
 
-vis1Door1Allow = 0
-vis1Door2Allow = 0
-vis2Door1Allow = 0
-vis2Door2Allow = 0
+global vis1Door1Allow
+global vis1Door2Allow
+global vis2Door1Allow
+global vis2Door2Allow
+vis1Door1Allow=0
+vis2Door1Allow=0
+vis1Door2Allow=0
+vis2Door2Allow=0
 window = tk.Tk()
-c = tk.Canvas(window, bg="white", width=total_x, height=total_y)
+c = tk.Canvas(window, bg="white", width=640, height=480)
 lblVis1 = tk.Label(window, bg='white', width=20)
 lblVis2 = tk.Label(window, bg='white', width=20)
 
@@ -64,21 +71,31 @@ def RefreshCanvas():
     c.delete("all")
 
     c.create_oval(item1.x - 4, item1.y - 4, item1.x + 4, item1.y + 4, fill="red")
-    c.create_oval(item1.x - 50, item1.y - 50, item1.x + 50, item1.y + 50)
+    c.create_oval(item1.x - 66, item1.y - 66, item1.x + 66, item1.y + 66)
+    c.create_text(item1.x-8, item1.y+12, fill="darkblue", text='Item 1')
 
     c.create_oval(item2.x - 4, item2.y - 4, item2.x + 4, item2.y + 4, fill="red")
-    c.create_oval(item2.x - 50, item2.y - 50, item2.x + 50, item2.y + 50)
-
+    c.create_oval(item2.x - 66, item2.y - 66, item2.x + 66, item2.y + 66)
+    c.create_text(item2.x-8, item2.y+12, fill="darkblue", text='Item 2')
+    
     c.create_oval(item3.x - 4, item3.y - 4, item3.x + 4, item3.y + 4, fill="red")
-    c.create_oval(item3.x - 50, item3.y - 50, item3.x + 50, item3.y + 50)
-
+    c.create_oval(item3.x - 66, item3.y - 66, item3.x + 66, item3.y + 66)
+    c.create_text(item3.x-8, item3.y+12, fill="darkblue", text='Item 3')
+    
     c.create_oval(door1.x - 4, door1.y - 4, door1.x + 4, door1.y + 4, fill="green")
-    c.create_oval(door1.x - 50, door1.y - 50, door1.x + 50, door1.y + 50)
+    c.create_oval(door1.x - 66, door1.y - 66, door1.x + 66, door1.y + 66)
+    c.create_text(door1.x+8, door1.y+12, fill="darkblue", text='Door 1')
+
+    c.create_oval(door2.x - 4, door2.y - 4, door2.x + 4, door2.y + 4, fill="green")
+    c.create_oval(door2.x - 66, door2.y - 66, door2.x + 66, door2.y + 66)
+    c.create_text(door2.x-8, door2.y+12, fill="darkblue", text='Door 2')
     
     if(visitor1.found):
         c.create_oval(visitor1.x - 4, visitor1.y - 4, visitor1.x + 4, visitor1.y + 4, fill="blue")
+        c.create_text(visitor1.x-8, visitor1.y+12, fill="darkblue", text='V1')
     if(visitor2.found):
         c.create_oval(visitor2.x - 4, visitor2.y - 4, visitor2.x + 4, visitor2.y + 4, fill="blue")
+        c.create_text(visitor2.x-8, visitor2.y+12, fill="darkblue", text='V2')
 
     lblVis1.config(text=esp1)
     lblVis2.config(text=esp2)
@@ -120,7 +137,7 @@ def track_everybody():
     v2_close_to = 0
     
     ret, img = cap.read()
-    width, height, channels = img.shape
+    height, width, channels = img.shape
 
     global esp1
     global esp2
@@ -146,13 +163,15 @@ def track_everybody():
                                         item1=item1,
                                         item2=item2,
                                         item3=item3,
-                                        door=door1)
+                                        door1=door1,
+                                        door2=door2)
 
         v2_close_to = choose_recordings(visitor2.x, visitor2.y,
                                         item1=item1,
                                         item2=item2,
                                         item3=item3,
-                                        door=door1)
+                                        door1=door1,
+                                        door2=door2)
         
         frame = cv2.putText(img,
                           'Visitor 1 close to: '+ str(v1_close_to),
@@ -163,11 +182,21 @@ def track_everybody():
                           'Visitor 2 close to: ' + str(v2_close_to),
                           (10, 40),
                           cv2.FONT_HERSHEY_SIMPLEX, 0.5, (34, 200, 34), 2)
+        frame = cv2.putText(img,
+                          'visit1alloweddoor1: '+ str(vis1Door1Allow) + 'visit1alloweddoor2: '+ str(vis1Door2Allow),
+                          (10, 60),
+                          cv2.FONT_HERSHEY_SIMPLEX, 0.5, (34, 200, 34), 2)
 
         if v1_close_to == 4: # door1
-            esp1 = "door1.aac" if vis1Door1Allow else "notAllowed.aac"
+            if vis1Door1Allow:
+                esp1 = "Allowed.m4a"
+            else:
+                esp1 = "notAllowed.m4a"
         elif v1_close_to == 5: # door2
-            esp1 = "door2.aac" if vis1Door2Allow else "notAllowed.aac"
+            if vis1Door2Allow:
+                esp1 = "Allowed.m4a"
+            else:
+                esp1 = "notAllowed.m4a"
         else:
             esp1 = "item" + str(v1_close_to) + ".m4a"
 
@@ -180,9 +209,15 @@ def track_everybody():
             lastEsp1 = esp1
 
         if v2_close_to == 4: # door1
-            esp1 = "door1.aac" if vis2Door1Allow else "notAllowed.aac"
+            if vis2Door1Allow:
+                esp2 = "Allowed.m4a"
+            else:
+                esp2 = "notAllowed.m4a"
         elif v2_close_to == 5: # door2
-            esp1 = "door2.aac" if vis2Door2Allow else "notAllowed.aac"
+            if vis2Door2Allow:
+                esp2 = "Allowed.m4a"
+            else:
+                esp2 = "notAllowed.m4a"
         else:
             esp2 = "item" + str(v2_close_to) + ".m4a"
 
@@ -205,3 +240,4 @@ GuiStart()
 th2.join()
 
 cv2.destroyAllWindows()
+
