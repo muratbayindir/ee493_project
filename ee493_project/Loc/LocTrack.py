0d94a3f773e1002f36f80e7485d5b0c5e913e7c5
Şamil Bayyurt <35709507+SamBayy@users.noreply.github.com>
2021-06-17 10:33:06 +0300

    Add files via upload
    en yeni dosyalar, esp2 visitor 3 e bağlı.

---
+++ ee493_project/Loc/LocTrack.py
@@ -0,0 +1,264 @@
+import cv2
+from threading import Thread
+import museum
+import math
+import tkinter as tk
+import threading
+import time
+
+def item_vs_visitors(item, visitor_x, visitor_y):
+    distance = 0.00
+    distance = (item.x-visitor_x)*(item.x-visitor_x)+(item.y-visitor_y)*(item.y-visitor_y)
+    distance = math.sqrt(distance)
+    if distance < 50*1.8:
+        return True
+    else:
+        return False
+
+
+def choose_recordings(x, y, item1, item2, item3, door1, door2):
+    close_to_this = 0
+    if item_vs_visitors(item1, x, y): # is it close to item1
+        close_to_this = 1  # esp1 = "item1.m4a"
+    elif item_vs_visitors(item2, x, y): # is it close to item2
+        close_to_this = 2  # esp1 = "item2.m4a"
+    elif item_vs_visitors(item3, x, y): # is it close to item3
+        close_to_this = 3  # esp1 = "item3.m4a"
+    elif item_vs_visitors(door1, x, y):
+        close_to_this = 4 # visitor at the door; play 'you are not allowed'.
+    elif item_vs_visitors(door2, x, y):
+        close_to_this = 5 # visitor at the door; play 'you are not allowed'.
+    else:
+        close_to_this = 0  # esp1 = "item2.m4a"  # no record will be played!!!!!!!!!!! it is not close to any item
+    return close_to_this
+
+
+###########################################
+total_x = 640  # this value must be changed
+total_y = 480  # this value must be changed
+###########################################
+
+item1 = museum.Items(47, 432)  # this values must be changed
+item2 = museum.Items(593, 432)  # this values must be changed
+item3 = museum.Items(320, 47)  # this values must be changed
+door1 = museum.Items(47, 47) #tam kosede
+door2 = museum.Items(593, 47) #unknown
+
+visitor1 = museum.Visitor('Visitor 1')
+visitor1.change_color('color1')
+
+visitor2 = museum.Visitor('Visitor 2')
+visitor2.change_color('color2')
+
+visitor3 = museum.Visitor('Visitor 3')
+visitor3.change_color('color3')
+
+window = tk.Tk()
+vis1Door1Allow = tk.IntVar()
+vis1Door2Allow = tk.IntVar()
+vis2Door1Allow = tk.IntVar()
+vis2Door2Allow = tk.IntVar()
+c = tk.Canvas(window, bg="white", width=total_x, height=total_y)
+lblVis1 = tk.Label(window, bg='white', width=20)
+lblVis2 = tk.Label(window, bg='white', width=20)
+
+esp1 = ""
+esp2 = ""
+
+def RefreshCanvas():
+
+    c.delete("all")
+
+    c.create_oval(item1.x - 4, item1.y - 4, item1.x + 4, item1.y + 4, fill="red")
+    c.create_oval(item1.x - 66, item1.y - 66, item1.x + 66, item1.y + 66)
+    c.create_text(item1.x-8, item1.y+12, fill="darkblue", text='Item 1')
+
+    c.create_oval(item2.x - 4, item2.y - 4, item2.x + 4, item2.y + 4, fill="red")
+    c.create_oval(item2.x - 66, item2.y - 66, item2.x + 66, item2.y + 66)
+    c.create_text(item2.x-8, item2.y+12, fill="darkblue", text='Item 2')
+    
+    c.create_oval(item3.x - 4, item3.y - 4, item3.x + 4, item3.y + 4, fill="red")
+    c.create_oval(item3.x - 66, item3.y - 66, item3.x + 66, item3.y + 66)
+    c.create_text(item3.x-8, item3.y+12, fill="darkblue", text='Item 3')
+    
+    c.create_oval(door1.x - 4, door1.y - 4, door1.x + 4, door1.y + 4, fill="green")
+    c.create_oval(door1.x - 66, door1.y - 66, door1.x + 66, door1.y + 66)
+    c.create_text(door1.x+8, door1.y+12, fill="darkblue", text='Door 1')
+
+    c.create_oval(door2.x - 4, door2.y - 4, door2.x + 4, door2.y + 4, fill="green")
+    c.create_oval(door2.x - 66, door2.y - 66, door2.x + 66, door2.y + 66)
+    c.create_text(door2.x-8, door2.y+12, fill="darkblue", text='Door 2')
+    
+    if(visitor1.found):
+        c.create_oval(visitor1.x - 4, visitor1.y - 4, visitor1.x + 4, visitor1.y + 4, fill="blue")
+        c.create_text(visitor1.x-8, visitor1.y+12, fill="darkblue", text='V1')
+    if(visitor2.found):
+        c.create_oval(visitor2.x - 4, visitor2.y - 4, visitor2.x + 4, visitor2.y + 4, fill="blue")
+        c.create_text(visitor2.x-8, visitor2.y+12, fill="darkblue", text='V2')
+    if(visitor3.found):
+        c.create_oval(visitor3.x - 4, visitor3.y - 4, visitor3.x + 4, visitor3.y + 4, fill="blue")
+        c.create_text(visitor3.x-8, visitor3.y+12, fill="darkblue", text='V3')
+
+    lblVis1.config(text=esp1)
+    lblVis2.config(text=esp2)
+
+def GuiClock():
+    RefreshCanvas()
+    window.after(100, GuiClock)
+
+def GuiStart():
+
+    chkVis1Door1Allow = tk.Checkbutton(window, text="Visitor 1 Door 1 Allow", variable=vis1Door1Allow, onvalue=1, offvalue=0)
+    chkVis1Door2Allow = tk.Checkbutton(window, text="Visitor 1 Door 2 Allow", variable=vis1Door2Allow, onvalue=1, offvalue=0)
+    chkVis2Door1Allow = tk.Checkbutton(window, text="Visitor 2 Door 1 Allow", variable=vis2Door1Allow, onvalue=1, offvalue=0)
+    chkVis2Door2Allow = tk.Checkbutton(window, text="Visitor 2 Door 2 Allow", variable=vis2Door2Allow, onvalue=1, offvalue=0)
+
+    lblVis1.pack()
+    lblVis2.pack()
+    chkVis1Door1Allow.pack()
+    chkVis1Door2Allow.pack()
+    chkVis2Door1Allow.pack()
+    chkVis2Door2Allow.pack()
+    c.pack()
+
+    GuiClock()
+    window.mainloop()
+
+def track_everybody():
+    # get the video
+    cap = cv2.VideoCapture(1)
+    if not cap.isOpened():  # for real time camera
+        print("sorun var aga")
+    
+    v1_close_to = 0
+    v2_close_to = 0
+    v3_close_to = 0
+    
+    ret, img = cap.read()
+    height, width, channels = img.shape
+
+    global esp1
+    global esp2
+
+    global vis1Door1Allow
+    global vis1Door2Allow
+    global vis2Door1Allow
+    global vis2Door2Allow
+    
+    f = open("files/esp1", "r")
+    esp1 = lastEsp1 = f.read()
+    f.close()
+
+    f = open("files/esp2", "r")
+    esp2 = lastEsp2 = f.read()
+    f.close()
+    #############################
+    #f = open("files/esp3", "r")#
+    #esp3 = lastEsp3 = f.read() #
+    #f.close()                  #
+    #############################
+    while cv2.waitKey(1) != 27: # ESC
+        ret, img = cap.read()
+        if img is None:
+            break
+            
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)  # Blue, Green, Red -> HSV coding
+        img = visitor1.track(img, hsv)
+        img = visitor2.track(img, hsv)
+        img = visitor3.track(img, hsv)
+        
+        v1_close_to = choose_recordings(visitor1.x, visitor1.y,
+                                        item1=item1,
+                                        item2=item2,
+                                        item3=item3,
+                                        door1=door1,
+                                        door2=door2)
+
+        v2_close_to = choose_recordings(visitor2.x, visitor2.y,
+                                        item1=item1,
+                                        item2=item2,
+                                        item3=item3,
+                                        door1=door1,
+                                        door2=door2)
+
+        v3_close_to = choose_recordings(visitor3.x, visitor3.y,
+                                        item1=item1,
+                                        item2=item2,
+                                        item3=item3,
+                                        door1=door1,
+                                        door2=door2)
+        line_holder = 0
+        if visitor1.found == True:
+            line_holder = line_holder + 20
+            frame = cv2.putText(img,
+                              'Visitor 1 close to: '+ str(v1_close_to),
+                              (10, line_holder),
+                              cv2.FONT_HERSHEY_SIMPLEX, 0.5, (34, 200, 34), 2)
+        if visitor2.found == True:
+            line_holder = line_holder + 20
+            frame = cv2.putText(img,
+                              'Visitor 2 close to: ' + str(v2_close_to),
+                              (10, line_holder),
+                              cv2.FONT_HERSHEY_SIMPLEX, 0.5, (34, 200, 34), 2)
+        if visitor3.found == True and visitor1.found == True and visitor2.found == True:
+            line_holder = line_holder + 20
+            frame = cv2.putText(img,
+                                'Maximum occupancy has been reached! Last Visitor is being warned..',
+                                (10, line_holder),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (34, 200, 34), 2)
+            #f = open("files/esp3", "w")
+            #f.write("maxOccupancy.m4a")
+            #f.close()
+            
+            #########################
+            if esp2 != lastEsp2:
+                f = open("files/esp2", "w")
+                f.write("maxOccupancy.m4a")
+                f.close()
+            
+            lastEsp2 = esp2
+            ########################
+
+        if v1_close_to == 4: # door1
+            esp1 = "door1.m4a" if vis1Door1Allow.get() else "notAllowed.m4a"
+        elif v1_close_to == 5: # door2
+            esp1 = "door2.m4a" if vis1Door2Allow.get() else "notAllowed.m4a"
+        else:
+            esp1 = "item" + str(v1_close_to) + ".m4a"
+
+        if esp1 != lastEsp1:
+
+            f = open("files/esp1", "w")
+            f.write(esp1)
+            f.close()
+            
+            lastEsp1 = esp1
+        
+
+        #if v2_close_to == 4: # door1
+        #    esp1 = "door1.m4a" if vis2Door1Allow.get() else "notAllowed.m4a"
+        #elif v2_close_to == 5: # door2
+        #    esp1 = "door2.m4a" if vis2Door2Allow.get() else "notAllowed.m4a"
+        #else:
+        #    esp2 = "item" + str(v2_close_to) + ".m4a"
+
+        #if esp2 != lastEsp2:
+
+         #   f = open("files/esp2", "w")
+         #   f.write(esp2)
+          #  f.close()
+            
+          #  lastEsp2 = esp2
+
+        cv2.imshow("Frame", img)
+
+    cap.release()
+
+th2 = Thread(target=track_everybody, args=())
+th2.start()
+
+GuiStart()
+th2.join()
+
+cv2.destroyAllWindows()
+
---
+++ ee493_project/Loc/museum.py
@@ -0,0 +1,127 @@
+import cv2
+from collections import deque
+import numpy as np
+
+colorNone = np.array([[0, 0, 0], [0, 0, 0]])
+# color1 = np.array([[180, 255, 30], [0, 0, 0]])
+# color1 = np.array([[27, 197, 255], [21, 107, 140]])
+color1 = np.array([[255, 224, 255], [73, 155, 205]])
+color2 = np.array([[55, 255, 255], [0, 25, 226]])
+color3 = np.array([[255, 255, 255], [150, 98, 59]])
+
+
+color_dict_HSV = {'None': colorNone,
+                  'color1': color1,
+                  'color2': color2,
+                  'color3': color3}
+
+
+###########################################
+total_x = 640  # this value must be changed
+total_y = 480  # this value must be changed
+###########################################
+
+def pixels_to_cms(current_pixel, all_pixels, total_length):
+    in_cms = (current_pixel/all_pixels) * total_length
+    return in_cms
+
+class Visitor:
+    def __init__(self,
+                 name=None,
+                 color=None,
+                 sections=None,
+                 visit_map=None):
+        if name is None:
+            name = 'None'
+        if color is None:
+            color = [[35, 255, 255], [25, 50, 70]]  # default color is yellow.
+        if sections is None:
+            sections = [True, True]
+        if visit_map is None:
+            visit_map = []
+        self.name = name
+        self.color = color
+        self.sections = sections
+        self.visit_map = visit_map
+        self.found = False
+        self.x = -500
+        self.y = -500
+
+    def change_color(self, new_color):
+        self.color = color_dict_HSV[new_color]
+
+    def track(self, frame, hsv):
+        x = -500
+        y = -500
+        pts = deque(maxlen=50)
+        krnl = np.ones((4, 4), np.uint8)  # burada yapıyor
+        # masking
+        mask = cv2.inRange(hsv, self.color[1], self.color[0])
+        # dilation and erode
+        mask = cv2.erode(mask, krnl, iterations=1)  # burada yapıyor
+        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, krnl)  # burada yapıyor
+
+        # orig        
+        # krnl = np.ones((5, 5), np.uint8)  # burada yapıyor
+        # # masking
+        # mask = cv2.inRange(hsv, self.color[1], self.color[0])
+        # # dilation and erode
+        # mask = cv2.erode(mask, krnl, iterations=2)  # burada yapıyor
+        # mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, krnl)  # burada yapıyor
+        # mask = cv2.dilate(mask, krnl, iterations=1)    # burada yapıyor
+
+        # finding contours
+        count, heir = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2:]
+        center = None
+        found = False
+
+        if len(count) > 0:
+            # Get the largest contour and its center
+            c = max(count, key=cv2.contourArea)
+            ((x, y), radius) = cv2.minEnclosingCircle(c)
+            m = cv2.moments(c)
+            center = (int(m["m10"] / m["m00"]), int(m["m01"] / m["m00"]))
+
+            # Allow only contours that have a larger than 5 pixel radius
+            if radius > 5:
+                # creating circle to the contour
+                text = "Location(in pixel) of " + str(self.name) + " -> x: " + str(int(x)) + " y: " + str(int(y))
+                frame = cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+                frame = cv2.circle(frame, center, 5, (0, 0, 255), -1)
+                found = True
+
+
+        self.found = found
+        self.x = x
+        self.y = y
+
+        # appending centers
+        pts.appendleft(center)
+        for i in range(1, len(pts)):
+            if pts[i - 1] is None or pts[i] is None:
+                continue
+
+            # displaying the traces
+            thick = int(np.sqrt(len(pts) / float(i + 1)) * 2.5)
+            frame = cv2.line(frame, pts[i - 1], pts[i], (0, 0, 225), thick)
+
+        return frame
+class Items:
+    def __init__(self,
+                 x=None,
+                 y=None,
+                 x_cm=None,
+                 y_cm=None,
+                 frame=None):
+        if x is None:
+            x = 0
+        if y is None:
+            y = 0
+        if x_cm is None:
+            x_cm = 0
+        if y_cm is None:
+            y_cm = 0
+        self.x_cm = (x/480) * total_x
+        self.y_cm = (y/640) * total_y
+        self.x = x
+        self.y = y
